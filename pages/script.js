d0a0af72f2b4e4eb77521bbe7bfa19a8314951f9
Praise Chukwunazaekpele Ameke <d24127929@myTUDublin.ie>
2024-12-02 22:57:04 +0000

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,5 +1,22 @@
 function submitForm() {
-    console.log("Submit button clicked!"); // Debugging line
+    console.log("Submit button clicked!");
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value;
+    
+    // avoiding blank input
+    if (name === '' || email === '') {
+    alert('Name and email are required’);
+    return false;
+    }
+    // validating email format using a simple regular expression
+    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+    alert('Please enter a valid email address’);
+    return false;
+    }
+    // if all validations pass
+    return true;
+    }// Debugging line
 
     const form = document.getElementById("contactForm");
     if (form) {
