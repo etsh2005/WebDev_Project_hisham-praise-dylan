d2946a5e3812059f10cf389ee5658b153f3635c0
Praise Chukwunazaekpele Ameke <d24127929@myTUDublin.ie>
2024-12-03 21:26:07 +0000

    Update script.js

--- pages/script.js
+++ pages/script.js
@@ -1,7 +1,5 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
 
-console.log("Try programiz.pro");
+
 
 function submitForm() {
     console.log("Submit button clicked!");
